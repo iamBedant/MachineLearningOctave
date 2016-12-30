3d7f4ce9b37b8cd2643339b72950d39d9447942d
iamBedant <bebi2527@live.in>
2016-12-30 11:31:16 +0530

    feat: Assignment for week 5 completed

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -64,21 +64,66 @@ Theta2_grad = zeros(size(Theta2));
 
 
 
+yb = zeros(m,num_labels);
+for i=1:m
+ yb(i,y(i)) = 1;
+end 
+X1 = [ones(m,1), X];
+
+z1 = X1 * Theta1';
+a1 = sigmoid(z1);
+a11 = [ones(m,1), a1];
+
+z2 = a11 * Theta2';
+a2 = sigmoid(z2);
+
+% J = 1/m(-y*log(h*x) - (1-y)log(1-h*x)) + (lambda/2m)(theta1^2+ theta2^2)
+J = sum(sum( -yb.*log(a2) - (1-yb).*log(1-a2) ))/m;
 
 
+% Add the regularization penalty
+J = J + (sum(sum(Theta1(:,2:end).^2)) + sum(sum(Theta2(:,2:end).^2)) )* lambda/(2*m);
+
+% -------------------------------------------------------------
+% Compute gradients using back propagation
+
+% Sample by sample
+for t = 1:m
+    % Forward to calculate error for sample t
+    a_1 = X(t,:)';
+    a_1 = [1; a_1];
+           
+    z_2 = Theta1 * a_1;
+    
+    a_2 = sigmoid(z_2);
+    a_2 = [1; a_2];
+    
+    z_3 = Theta2 * a_2;
+    
+    a_3 = sigmoid(z_3);
+    
+    % Error
+    yt = yb(t,:)';
+    delta_3 = a_3 - yt; 
+    
+    % Propagate error backwards
+    delta_2 = (Theta2' * delta_3) .* sigmoidGradient([1; z_2]);
+    delta_2 = delta_2(2:end); 
+
+    dt2 = delta_3 * a_2';
+    dt1 = delta_2 * a_1';
+ 
+    Theta2_grad = Theta2_grad + dt2;
+    Theta1_grad = Theta1_grad + dt1;
+end
 
 
+Theta1_grad = (1/m) * Theta1_grad;
+Theta2_grad = (1/m) * Theta2_grad ;
 
-
-
-
-
-
-
-
-
-
-
+% Add regularization terms
+Theta1_grad(:,2:end) = Theta1_grad(:,2:end) + (lambda/m) * Theta1(:,2:end);
+Theta2_grad(:,2:end) = Theta2_grad(:,2:end) + (lambda/m) * Theta2(:,2:end);
 
 % -------------------------------------------------------------
 
